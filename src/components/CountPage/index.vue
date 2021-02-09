bd4e97ff0b2fa198cf23b37843f9b366a198337a
herouu <2269648132@qq.com>
2021-02-09 16:27:49 +0800

    更改按钮颜色

---
+++ src/components/CountPage/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button v-on:click="count++">You clicked me {{ count }} times.</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CountPage",
+  props: {
+    count: Number
+  },
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
